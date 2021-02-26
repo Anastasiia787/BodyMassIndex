ba6db4a53aa037ad5b6a4f950b78531d5df52c6a
Anastasia <nastenaprokopeva78@gmail.com>
2021-02-26 23:25:27 +0300

    задание переменных

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BodyMassIndexService service = new BodyMassIndexService();
-        double BMI = service.calculate(50, 150);
+        double weight = 50;
+        double heightSm = 150;
+        double BMI = service.calculate(weight, heightSm);
         System.out.printf("%.1f",BMI);
     }
 }
